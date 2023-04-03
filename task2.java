28fc92a08ec117afb1ca37606251ddb292838443
Luba <bart6891@yandwx.ru>
2023-04-03 11:25:15 +0300

    2

---
+++ task2.java
@@ -0,0 +1,47 @@
+
+import java.util.LinkedList;
+
+// 2. Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди, 
+// dequeue() - возвращает первый элемент из очереди и удаляет его, 
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+public class task2 {
+
+    public static void main(String[] args) {
+
+        int[] list = { 2, 8, 1, 4, 5, 9 };
+
+        LinkedList<Integer> linkList = new LinkedList<>();
+        for (Integer temp : list) {
+            linkList.add(temp);
+        }
+
+        System.out.println(linkList);
+
+        enqueue(linkList, 7);
+        System.out.println(linkList);
+
+        System.out.println(dequeue(linkList));
+        System.out.println(linkList);
+
+        System.out.println(first(linkList));
+    }
+
+    public static void enqueue(LinkedList<Integer> list, int num) {
+        list.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        list.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> list) {
+        int num = 0;
+        num = list.get(0);
+        return num;
+    }
+}
